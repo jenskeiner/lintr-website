6a03a374f84e86af512b04bd38fc844761c3b0cf
Jens Keiner <jens.keiner@gmail.com>
2025-03-16 20:05:47 +0100

    Bump some package versions.

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: [
     require("postcss-import"),
     require("tailwindcss"),
     require("autoprefixer"),
     require("postcss-nested"),
-  ],
+  ]
 }
+
+module.exports = config
